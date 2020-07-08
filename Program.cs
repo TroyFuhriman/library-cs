5796eeaa5f2296bcd5ad8083e16b6a6e162ce779
Troy Fuhriman <TkFuhriman@gmail.com>
2020-07-08 14:53:15 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using console_library.Models;
+
+namespace library
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Book wherethesidewalkends = new Book("where the sidewalk ends", "Shel Silverstein");
+    }
+  }
+}
